78fa13a176b232a466f3edd6efed1518cc5d2cdd
ravcreate <ravuthvann@outlook.com>
2019-08-25 04:39:59 -0700

    Updated Copy on Index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Header from "../components/header"
 export default () => (
     <div style={{color:`purple`}}>
         <Link to="/contact/">Contact</Link>
-        <Header headerText="Welcome to Gatsby."/>
+        <Header headerText="Welcome to a Gatsby Introduction."/>
         <p>What a world.</p>
         <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
